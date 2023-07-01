ad7696f1c7d519110ab029ca9b9d366749bbe60a
linuxmobile <bdiez19@gmail.com>
2023-07-01 00:02:12 -0300

    :sparkles: Adding hero & more
    Adding Hero Section, Btn components, event component & grid bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,19 +13,45 @@ module.exports = {
 				lg:
 					'0px 25px 7px rgba(24, 24, 27, 0.01), 0px 16px 6px rgba(24, 24, 27, 0.04), 0px 9px 5px rgba(24, 24, 27, 0.15), 0px 4px 4px rgba(24, 24, 27, 0.26), 0px 1px 2px rgba(24, 24, 27, 0.29), 0px 0px 0px rgba(24, 24, 27, 0.3);',
 			},
-      colors: {
-        'midu-50': '#E5F5FF',
-        'midu-100': '#B3E0FF',
-        'midu-200': '#80CCFF',
-        'midu-300': '#4DB8FF',
-        'midu-400': '#0099FF',
-        'midu-500': '#0063C6',
-        'midu-600': '#031931',
-        'midu-700': '#00111C'
-      },
-      fontFamily: {
-        'midufont': ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol']
-      }
+			colors: {
+				'midu-50': '#E5F5FF',
+				'midu-100': '#B3E0FF',
+				'midu-200': '#80CCFF',
+				'midu-300': '#4DB8FF',
+				'midu-400': '#0099FF',
+				'midu-500': '#0063C6',
+				'midu-600': '#031931',
+				'midu-700': '#00111C',
+				'midu-purple': '#7C3AED',
+			},
+			fontFamily: {
+				'midufont': [
+					'Roboto',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Helvetica',
+					'Arial',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+				],
+			},
+			textWrap: {
+				'balance': 'balance',
+			},
+			fontStretch: {
+				'ultra-condensed': 'ultra-condensed',
+				'extra-condensed': 'extra-condensed',
+				'condensed': 'condensed',
+				'semi-condensed': 'semi-condensed',
+				'normal': 'normal',
+				'semi-expanded': 'semi-expanded',
+				'expanded': 'expanded',
+				'extra-expanded': 'extra-expanded',
+				'ultra-expanded': 'ultra-expanded',
+			},
 		},
 	},
 	plugins: [],

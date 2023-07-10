469267b065daac0bec965083a44482dc69efc923
linuxmobile <bdiez19@gmail.com>
2023-07-09 22:39:03 -0300

    :sparkles: Added readtime to articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import { remarkReadingTime } from './src/services/readTime.js'
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      theme: 'rose-pine',
-      wrap: true,
-    },
-  },
+	markdown: {
+		shikiConfig: {
+			theme: 'rose-pine',
+			wrap: true,
+		},
+		remarkPlugins: [remarkReadingTime],
+	},
 	integrations: [tailwind(), mdx()],
 })
